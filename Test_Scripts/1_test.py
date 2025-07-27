0aeb6123284a860dd7302fd74dc3b77aa87cdf74
Santhosh R <san.sathish.com@gmail.com>
2025-07-28 03:59:06 +0530

    Modified 1_test

--- Test_Scripts/1_test.py
+++ Test_Scripts/1_test.py
@@ -1,3 +1,11 @@
+from Utils.WebAppDriver import WebAppDriver
+
+
 def test_1():
     print("Hello")
+    app = WebAppDriver("https://facebook.com")
+    app.launch()
+    print(app.get_element_by_text("Log in").text)
+    app.close()
+    print("Done")
 
