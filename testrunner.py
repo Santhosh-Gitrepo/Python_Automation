ed7faff477922dd2f5430800d246a7dc34d945b8
Santhosh R <san.sathish.com@gmail.com>
2025-08-18 14:38:59 +0530

    updated

--- testrunner.py
+++ testrunner.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+
+import pytest
+
 import report_generator
 
 REPORT_DIR = "reports_temp"  # Temporary folder for pytest report in automation repo
@@ -10,7 +13,8 @@ PYTEST_CMD = ["pytest", f"--html={os.path.join(REPORT_DIR, REPORT_FILE)}", "--se
 def run_pytest():
     os.makedirs(REPORT_DIR, exist_ok=True)
     print("Running pytest and generating report...")
-    subprocess.run(PYTEST_CMD)
+    # subprocess.run(PYTEST_CMD)
+    subprocess.run(PYTEST_CMD, shell=True)
     print("Pytest completed.")
 
 
